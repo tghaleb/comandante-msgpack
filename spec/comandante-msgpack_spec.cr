b367681108af4cd362abfeee745f20d817083fed
Tarek Ghaleb <tghaleb@users.noreply.github.com>
2021-11-29 17:28:46 +0000

    cleanup in Makefile and specs

--- spec/comandante-msgpack_spec.cr
+++ spec/comandante-msgpack_spec.cr
@@ -3,18 +3,24 @@ require "./spec_helper"
 include Comandante
 
 describe Comandante::Helper do
-  # should we have stream reader as well?
-  it "reading should work" do
+  it "reading single object work" do
     data_a = [1, 2, 3]
-    data_b = [3, 2, 3]
-    results = Array(Array(Int32)).new
 
     Cleaner.tempfile do |f|
       Helper.write_msgpack(data_a, f)
 
       data = Helper.read_msgpack(f, Array(Int32).new)
       data.should eq(data_a)
+    end
+  end
 
+  it "reading multiple objects should work" do
+    data_a = [1, 2, 3]
+    data_b = [3, 2, 3]
+    results = Array(Array(Int32)).new
+
+    Cleaner.tempfile do |f|
+      Helper.write_msgpack(data_a, f)
       Helper.write_msgpack(data_b, f, mode = "a")
 
       Helper.read_msgpack(f, Array(Int32).new) do |data|
